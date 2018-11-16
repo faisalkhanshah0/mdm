f8bb107f71c716f054b12839906641bf5b263b64
faisal <faisal@faisal-ubuntu.igglobal.com>
2018-11-16 16:00:50 +0530

    files added

--- services/router.js
+++ services/router.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = new express.Router();
 const company_id = require('../controllers/get_cmat_info.js');
+const alt_es = require('../controllers/get_altavault_eseries.js');
 //const company_cmat_id = require('../controllers/get_ib_info.js');
 
 // Company details routes starts  
 router.route('/')
   .get(company_id.get);
 // Company details routes ends  
+
+// Get Altavault routes starts  
+router.route('/getaltavault')
+  .get(alt_es.get_altavault);
+// Get Altavault routes ends 
+
 
 /*
 router.route('/company_cmat_id/:id?')
